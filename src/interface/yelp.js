21d27718f9a6fbf93dd639125960c09c3041b8e9
Andres Weber <andresmweber@gmail.com>
2019-08-26 11:56:04 -0400

    Added basic h1 to index.html.  Added group routing.  Added restaurants routing.  Added users routing.

--- src/interface/yelp.js
+++ src/interface/yelp.js
@@ -1,13 +1,13 @@
 'use strict';
- 
+
 const yelp = require('yelp-fusion');
 const client = yelp.client(process.env.LUNCHMEMO_YELP_API_KEY);
- 
+
 client.search({
-  term: 'Four Barrel Coffee',
-  location: 'san francisco, ca',
+    term: 'Four Barrel Coffee',
+    location: 'san francisco, ca',
 }).then(response => {
-  console.log(response.jsonBody.businesses[0]);
+    console.log(response.jsonBody.businesses[0]);
 }).catch(e => {
-  console.log(e);
+    console.log(e);
 });
