80126f827d6519e9d32f397983129fbe36554575
Andres Weber <andresmweber@gmail.com>
2019-08-26 13:44:09 -0400

    Fixed pathing to db.json. Fixed issues with database querying not using async/await.

--- src/db/database.js
+++ src/db/database.js
@@ -1,13 +1,14 @@
+const path = require('path')
 const jsonServer = require('json-server');
 const dbServer = jsonServer.create();
-const dbRouter = jsonServer.router('./db.json');
+const dbRouter = jsonServer.router(path.join(__dirname, 'db.json'));
 const middleware = jsonServer.defaults();
 const port = process.env.DB_PORT || 3001;
 
 dbServer.use(middleware);
 dbServer.use(dbRouter);
 dbServer.listen(port, () => {
-    console.log('JSON Server is running')
+    console.log(`JSON Server is running on port ${port}`)
 });
 
 module.exports = dbServer
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port);
+    console.log(`Express server is up on port ${port}`);
 });
