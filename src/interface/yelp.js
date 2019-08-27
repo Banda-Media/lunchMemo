a050829d1361021cc724bf15cc2946b824407722
Andres Weber <andresmweber@gmail.com>
2019-08-27 10:11:57 -0400

    Finished Singleton Interface for YelpAPI and wrote autodoc.

--- src/interface/yelp.js
+++ src/interface/yelp.js
@@ -1,13 +1,45 @@
 'use strict';
+/**
+ * Singleton Interface for the Yelp Fusion API.  Must 
+ * MUST have defined your API Key for process.env as LUNCHMEMO_YELP_API_KEY
+ * @example 
+ * var yelpAPI = YelpAPI.getInstance();
+ * yelpAPI.search('coffee', 'new york, ny') // Returns a Promise.
+ */
+var YelpAPI = (function() {
+    var instance;
 
-const yelp = require('yelp-fusion');
-const client = yelp.client(process.env.LUNCHMEMO_YELP_API_KEY);
+    function createInstance() {
+        const yelp = require('yelp-fusion');
+        const client = yelp.client(process.env.LUNCHMEMO_YELP_API_KEY);
+        var object = {
+            api: yelp,
+            client: client,
+            search: async function(searchTerms, location) {
+                return await this.client.search({
+                        term: searchTerms,
+                        location: location
+                    })
+                    .then(res => { return res.jsonBody.businesses })
+                    .catch(e => {
+                        console.log(e)
+                        return e
+                    })
+            }
 
-client.search({
-    term: 'Four Barrel Coffee',
-    location: 'san francisco, ca',
-}).then(response => {
-    console.log(response.jsonBody.businesses[0]);
-}).catch(e => {
-    console.log(e);
-});
+        }
+
+        return object;
+    };
+
+    return {
+        getInstance: function() {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+module.exports = { YelpAPI }
