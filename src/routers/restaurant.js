72f959cce207c1d8e16b83a64c9199517b60e893
Andres Weber <andresmweber@gmail.com>
2019-08-27 10:42:52 -0400

    Added documentation for the yelp restaurant model object and added docuemntation to our restaurant router.  Also added /API/search/restaurants endpoint.

--- src/routers/restaurant.js
+++ src/routers/restaurant.js
@@ -1,16 +1,21 @@
 const express = require('express')
-const Restaurant = require('../models/restaurant')
+const { YelpAPI } = require('../interface/yelp')
 const router = new express.Router()
 
-router.post('/restaurants', async(req, res) => {
-    const user = new User(req.body)
+/**
+ * Route for handling yelp API searching for nearby restaurants
+ * MUST have defined your API Key for process.env as LUNCHMEMO_YELP_API_KEY
+ * @param {string} searchTerm - The type of restaurant you're looking for
+ * @param {string} location - The location to query (can be comma separated or just one name as you'd type into yelp) 
+ * @returns {object[]} - An array of yelp restaurant objects.
+ */
+router.post('/API/search/restaurants', async(req, res) => {
     try {
-        await user.save()
-        res.status(201).send({ user })
+        let yelpRestaurants = await YelpApi.getInstance().search(req.params.searchTerms, req.params.location)
+        res.status(201).send({ yelpRestaurants })
     } catch (e) {
         res.status(400).send(e)
     }
 })
-
 
 module.exports = router
