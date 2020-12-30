74977fdaa75b681dd3ebcd6ad9c4af34ad825a9a
Andres Weber <andresmweber@gmail.com>
2020-12-29 23:43:03 -0500

    Verifying admin.initializeApp functions by default.

--- app/utils/firebase/admin.ts
+++ app/utils/firebase/admin.ts
@@ -2,9 +2,7 @@ import * as admin from 'firebase-admin';
 
 const getFirebaseAdmin = async () => {
   if (!admin.apps.length) {
-    await admin.initializeApp({
-      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
-    });
+    await admin.initializeApp();
   }
 
   return admin;
