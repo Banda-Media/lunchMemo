72f6992aade2839e26eb37bc26cec1db38c5f37e
Andres Weber <andresmweber@gmail.com>
2021-01-26 14:23:22 -0500

    Added api endpoint.

--- app/services/firebase/admin.ts
+++ app/services/firebase/admin.ts
@@ -1,18 +1,21 @@
-import firebase from 'firebase-admin';
+import admin from 'firebase-admin';
 
-const getFirebaseAdmin = (): firebase.app.App => {
-  if (!firebase.apps.length) {
-    const admin = firebase.initializeApp({
-      credential: firebase.credential.cert({
+const getFirebaseAdmin = (): admin.auth.Auth => {
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
         projectId: process.env.NEXT_PUBLIC_LM_FIREBASE_PROJECT_ID,
         privateKey: process.env.NEXT_PUBLIC_LM_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         clientEmail: process.env.NEXT_PUBLIC_LM_FIREBASE_CLIENT_EMAIL
       }),
       databaseURL: process.env.NEXT_PUBLIC_LM_FIREBASE_DATABASE_URL
     });
-    return admin;
   }
-  throw new Error('Could not load firebase admin.');
+  try {
+    return admin.auth();
+  } catch (error) {
+    throw new Error(`Could not initialize Firebase Admin, ${JSON.stringify(error)}`);
+  }
 };
 
 export default getFirebaseAdmin;
